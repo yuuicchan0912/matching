a284139d26bf530a1fb9dbbd58ba90412c2a3ece
yuuicchan0912 <yuuicchan0912@gmail.com>
2022-11-19 21:46:22 +0900

    vuetifyのデフォルトテーマをダークテーマから変更して、カラーを追加

--- Docker-Nuxtjs-Express/frontend/nuxt.config.js
+++ Docker-Nuxtjs-Express/frontend/nuxt.config.js
@@ -82,7 +82,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -93,6 +93,15 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
+      },
+      light: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3
       }
     }
   },
